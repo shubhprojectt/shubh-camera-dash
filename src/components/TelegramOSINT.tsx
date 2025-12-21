5a65c256d56ecf9590d7dd3a93b335c14549deb9
Chaudhary <shubhchaudharyji99@gmail.com>
2025-12-21 20:20:06 +0530

    Update TelegramOSINT.tsx

--- src/components/TelegramOSINT.tsx
+++ src/components/TelegramOSINT.tsx
@@ -487,7 +487,7 @@ const TelegramOSINT: React.FC = () => {
       )}
 
       {/* Tools Label */}
-      <h2 className="text-gray-400 text-lg font-semibold mb-4">tele dark</h2>
+      <h2 className="text-gray-400 text-lg font-semibold mb-4">TELEGRAM ID DALO FULL MSG BNDA KIS GROUP ME JOIN HAI SARE MSG AND SIR BHI BAHUT KUCHH NIKALO</h2>
 
       {/* Tools Grid */}
       <div className="bg-black/40 border border-gray-800 rounded-xl p-3 mb-6">
